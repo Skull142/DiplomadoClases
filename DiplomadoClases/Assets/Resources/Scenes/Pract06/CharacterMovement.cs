a787daa4623abd5efae015ea52dc411e23abd341
Joaquin Ondarza <jnondarza@gmail.com>
2015-11-28 18:55:27 -0600

    clase04casa    prac06 terminada

--- DiplomadoClases/Assets/Resources/Scenes/Pract06/CharacterMovement.cs
+++ DiplomadoClases/Assets/Resources/Scenes/Pract06/CharacterMovement.cs
@@ -6,43 +6,50 @@ public class CharacterMovement : MonoBehaviour {
 	[HideInInspector] public Vector3 movementVector = Vector3.one;
 	public  float speed = 10f;
 	public float downSpeed = -10;
-	private bool jump;
 	public float jumpForce = 1f;
+	public float tJump = 1f;
+	//
+	[HideInInspector] public bool jump;
 	private CharacterController character;
 	private Rigidbody body;
+	private float counterJump;
 
 	void Start () 
 	{
 		character = this.GetComponent<CharacterController> ();
 		body = this.GetComponent<Rigidbody> ();
 		jump = false;
+		counterJump = 0f;
 	}
 
 	void Update () 
 	{
 		movementVector = Vector3.zero;
-		if (!character.isGrounded) 
-		{
-			character.SimpleMove ((this.transform.up) * (downSpeed) * Time.deltaTime);
-			if(jump)
-			{
-
-			}
-
-		}
-
-		if (character.isGrounded && Input.GetKeyDown (KeyCode.Space))
+		//
+		if (Input.GetKeyDown (KeyCode.Space) && !jump)
 			jump = true;
+		//
+		if(this.jump)
+		{
+			counterJump += Time.deltaTime;
+			if(counterJump >= tJump && jump)
+			{
+				this.counterJump = 0f;
+				this.jump = false;
+			}
+		}
+		//
 		Movement ();
-		character.Move ( this.transform.forward * movementVector.z );
 		character.Move ( this.transform.right * movementVector.x );
+		character.Move ( this.transform.up * movementVector.y );
+		character.Move ( this.transform.forward * movementVector.z );
 	}
 
 	void Movement()
 	{
 		movementVector.x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
 		movementVector.z = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
-		movementVector.y = downSpeed * Time.deltaTime;
+		movementVector.y = jump ? jumpForce : downSpeed * Time.deltaTime;
 	}
 
 	void OnControllerColliderHit(ControllerColliderHit cch)
