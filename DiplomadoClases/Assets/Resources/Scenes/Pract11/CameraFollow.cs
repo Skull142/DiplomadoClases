187597115abefef79dfabb7659fc91d9fbc5c146
Joaquin Ondarza <jnondarza@gmail.com>
2016-01-08 14:44:36 -0600

    Clase09 casaA

---
+++ DiplomadoClases/Assets/Resources/Scenes/Pract11/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject target;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.position = this.target.transform.position+(new Vector3(0f, 1f, -10f));
+	}
+}
---
+++ DiplomadoClases/Assets/Resources/Scenes/Pract11/Swipe.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swipe : MonoBehaviour {
+
+	public float minSwipeDistY;
+	public float minSwipeDistX;
+	//
+	private Vector2 startPos;
+	private Animator animator;
+
+	void Start()
+	{
+		this.animator = this.GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		#if !UNITY_STANDALONE
+			this.CheckSwipe();
+		#endif
+		#if UNITY_STANDALONE
+			this.CheckArrow();
+		#endif
+		this.transform.position = new Vector3(0, this.transform.position.y, 0f);
+		this.transform.eulerAngles = new Vector3(0, 180f, 0f);
+	}
+	private void CheckArrow()
+	{
+		Vector2 arrow = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		if(arrow.x > 0)				this.Right();
+		else if	(arrow.x < 0)		this.Left();
+		//
+		if(arrow.y > 0)	this.Up();
+		else if	(arrow.y < 0)		this.Down();
+	}
+	private void CheckSwipe()
+	{
+		if (Input.touchCount > 0) 
+		{
+			Touch touch = Input.touches[0];
+			switch (touch.phase) 
+			{
+				case TouchPhase.Began:
+					startPos = touch.position;
+				break;
+				case TouchPhase.Ended:
+					float swipeDistVertical = (new Vector3(0, touch.position.y, 0) - new Vector3(0, startPos.y, 0)).magnitude;
+					float swipeDistHorizontal = (new Vector3(touch.position.x,0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+					if (swipeDistVertical > minSwipeDistY) 
+					{
+						float swipeValue = Mathf.Sign(touch.position.y - startPos.y);
+						if (swipeValue > 0)//up swipe
+							Up();
+						else if (swipeValue < 0)//down swipe
+							Down();
+					}
+					if (swipeDistHorizontal > minSwipeDistX) 
+					{
+						float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+						if (swipeValue > 0)//right swipe
+							Right ();
+						else if (swipeValue < 0)//left swipe
+							Left ();
+					}
+				break;
+			}
+		}
+	}
+	private void Up()
+	{
+		this.animator.SetTrigger("UP");
+	}
+	private void Down()
+	{
+		this.animator.SetTrigger("DOWN");
+	}
+	private void Right()
+	{
+		//this.animator.SetTrigger("RIGHT");
+		print("Right");
+	}
+	private void Left()
+	{
+		//this.animator.SetTrigger("LEFT");
+		print("Left");
+	}
+}
